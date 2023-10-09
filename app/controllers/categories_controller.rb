36e5ac4a4c7eccdaba616d87928617eb8b8c2e2b
Elena Rocha <elena.rocha@globaleduca.com>
2023-10-09 10:04:44 +0200

    intentando reajustar la bśuqueda por géneros ahora que cada libro puede tener más de uno

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,17 @@
 class CategoriesController < ApplicationController
     
-    # GET categories/genre
+    # GET categories/genre/:genre
     def get_books_genre
-        @book = Book.where("genre_id = ?", params[:genre])
+        # @book = Book.where("genre_id = ?", params[:genre])
+
+        # @book = Book.where("book.genres = ?", params[:genre])
+        # @book = Book.where(book.genres.collect(&:definition).include?('params[:genre]'))
+        # @book = Book.find_by_genres(params[:genre])
+        #find_by_sql
+        
     end
 
-    # GET categories/author
+    # GET categories/search
     def get_books_search
         @books = @query.result(distinct: true)
     end
