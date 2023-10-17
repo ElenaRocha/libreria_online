c49b84a633dc27e7d1de0b5d25c9663011ee62ed
Elena Rocha <elena.rocha@globaleduca.com>
2023-10-17 10:13:34 +0200

    limpiando de comentarios

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,34 @@
 class CategoriesController < ApplicationController
-    
+    # before_action :clear_search, :only => [:get_books_search]
+
     # GET categories/genre/:genre
     def get_books_genre
-
         @books = Book.find_by_sql("SELECT * FROM books 
             INNER JOIN books_genres 
             ON books.id = books_genres.book_id 
             WHERE books_genres.genre_id = #{params[:genre]};")
-        puts "veamos el/los libros: #{@books}"
-        
     end
 
     # GET categories/search
     def get_books_search
+        # @query = Book.ransack(params[:q])
         @books = @query.result(distinct: true)
+        # puts "la query: #{@query} y el inspect: #{@query.inspect} y cuales son los params? #{params}"
+        params.delete("q")
+        # respond_to do |format|
+        #     format.html
+        #     format.json {render json: @books}
     end
-    
+
+    # def clear_search
+    #     if params[:q]
+    #         params.delete(:q)
+            # if(!search_params.nil?)
+            #     search_params.each do |key, param|
+            #         search_params[key] = nil
+            #     end
+            # end
+    #     end
+    # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
   resources :orders, :editions, :books, :authors, :genres, :users
-  # resources :editions
-  # resources :books
-  # resources :authors
-  # resources :genres
-  # resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  #métodos GET, POST, PATCH, PUT y DELETE
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   root to: "books#index"
 
