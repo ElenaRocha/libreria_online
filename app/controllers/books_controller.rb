530e9f4c218da491cb8e007d9094fe4881ca4073
Elena Rocha <elena.rocha@globaleduca.com>
2023-10-26 14:15:22 +0200

    enésimo intento de ajax

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -49,11 +49,12 @@ class BooksController < ApplicationController
 
   # DELETE /books/1 or /books/1.json
   def destroy
-    @book.destroy
+    # @book.destroy
 
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
       format.json { head :no_content }
+      format.js {}
     end
   end
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,13 @@ import "controllers"
 
 //= require jquery
 //= require jquery_ujs
+
+import "@hotwired/turbo-rails"
+Turbo.session.drive = false
+
+// ‘’’ data: { turbo: true } ‘’’
+
+// $(document).on('turbo:load', function (){ alert("turbolinks on load event works") });
+
+// esto antes estaba en el destroy.js.erb
+$('#books').append("<%= j render partial: 'hola' %>")
